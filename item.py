ff2eed6baf9ae2b68eb8bb36f5eecce20c447f93
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-11-13 14:45:52 -0600

    Item class fixed

--- item.py
+++ item.py
@@ -5,10 +5,31 @@ class Item:
         self.value = value
         self.name = name
 
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight):
+        if capacity <= 0:
+            raise ValueError('Weight must be positive')    
+        else:
+            self._weight = weight
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value < 0:
+            raise ValueError('Value must be positive')    
+        else:
+            self._value = value
+
+        
     def __str__(self):
         return f'Item {self.name}, with weight: {self.weight} and value: {self.value}'
 
-    def get_info(self):
-        return [self.weight, self.value, self.name]
-    
+  
 
