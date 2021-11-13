f43e4b0ebaa2bc7749ae2945cb4fd9da8a47991e
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-11-13 16:46:04 -0600

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,9 +1,9 @@
 from item import Item
 # Backpack needs capacity, an optimal value, and a list of accepted items.
 class Knapsack:
-    def __init__(self, capacity):
+    def __init__(self, capacity,n):
         self.capacity = capacity
-        self.total = 3
+        self.total = n
         self.opt_value = 0
         self.items = []
 
@@ -25,6 +25,18 @@ class Knapsack:
             raise ValueError('Optimal value must be positive')    
         else:
             self._opt_value = opt_value
+
+    
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, total):
+        if total <= 0:
+            raise ValueError('Optimal value must be positive')    
+        else:
+            self._total = total
         
     def __str__(self):
         return f'''Backpack with capacity of : {self.capacity} 
